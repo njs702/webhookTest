2c67eb7bc6173180288086cedd10158d2eafc1bc
Junseok <njs702@ajou.ac.kr>
2021-07-13 14:12:17 +0900

    test

--- helloworld/src/main/java/kr/ac/ajou/coding/App.java
+++ helloworld/src/main/java/kr/ac/ajou/coding/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!" );
-        System.out.println( "Jenkins notification test" );
+        System.out.println( "Jenkins notification test0713" );
     }
 }
